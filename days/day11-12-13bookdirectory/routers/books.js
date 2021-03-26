584420a20970c6943f5f0411cc31aa8d555ae3ea
Fernadno Aldair <ferpool_ald@hotmail.com>
2021-03-25 22:30:48 -0600

    day 13 add mongo

---
+++ days/day11-12-13bookdirectory/routers/books.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const books = require("../utils/books");
+const BooksService = require("../services/books");
+
+function booksApi(app) {
+  app.use("/api/books", router);
+
+  const booksService = new BooksService();
+
+  router.get("/", async (req, res, next) => {
+    const { tags } = req.query;
+    try {
+        const book = await booksService.getBooks({tags});
+        res.status(200).json({
+            data: book,
+        });
+    } catch (error) {
+        next(error);
+    }
+  });
+
+
+  router.get("/:bookId", async (req, res, next) => {
+    const { bookId } = req.params;
+    try {
+        const book = books.find(element => element.id = bookId);
+        res.status(200).json({
+            data: book,
+        });
+    } catch (error) {
+        next(error);
+    }
+  });
+
+  router.post("/", async (req, res, next) => {
+
+    const { book } = req.body;
+
+    try {
+        res.status(201).json({
+            data: book,
+        });
+    } catch (error) {
+        next(error);
+    }
+  });
+
+  router.put("/:bookId", async (req, res, next) => {
+    const { bookId } = req.params;
+    const { book } = req.body;
+    try {
+        const book = books.find(element => element.id = bookId);
+        res.status(200).json({
+            data: book,
+        });
+    } catch (error) {
+        next(error);
+    }
+  });
+
+  router.delete("/:bookId", async (req, res, next) => {
+    const { bookId } = req.params;
+    try {
+        const book = books.find(element => element.id = bookId);
+        res.status(200).json({
+            data: book,
+        });
+    } catch (error) {
+        next(error);
+    }
+  });
+
+
+
+}
+
+module.exports = booksApi;
